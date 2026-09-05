84ef66d3e7625ad9bff6c83a34a4052e19b50994
Tema Iakovlev <aryakovlev@x5.ru>
2026-09-05 16:32:56 +0300

    Новая кафка логика и все такое

--- Back/infra/kafka/publishers.py
+++ Back/infra/kafka/publishers.py
@@ -13,12 +13,6 @@ LLM_RESPONSE_TOPIC = os.getenv("LLM_RESPONSE_TOPIC")
 if not LLM_REQUEST_TOPIC:
     raise RuntimeError("LLM_REQUEST_TOPIC is not configured")
 
-async def publish_test_message(message: str) -> None:
-    await broker.publish(
-        message,
-        topic=LLM_REQUEST_TOPIC,
-    )
-
 async def publish_message(message: NewMessageEvent) -> None:
     try:
         await broker.publish(
